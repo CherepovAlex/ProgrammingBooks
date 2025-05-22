a6bbd8f21a6920e3c53c8a7aed2d7d92c6d2cb0e
CherepovAlex <westler123@yandex.ru>
2025-05-22 17:23:27 +0300

    Commit m4 p8 th7-3

--- Hexlet/Modul4/08-Java-Web-Technology/17-Middlewares-p3/src/main/java/io/hexlet/App.java
+++ Hexlet/Modul4/08-Java-Web-Technology/17-Middlewares-p3/src/main/java/io/hexlet/App.java
@@ -2,11 +2,21 @@ package io.hexlet;
 
 import io.javalin.Javalin;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class App {
 
     public static Javalin getApp() {
 
         var app = Javalin.create();
+
+        // Миддлвара для логирования даты и времени запроса
+        app.before(ctx -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String timestamp = LocalDateTime.now().format(formatter);
+            System.out.println("[" + timestamp + " ] Request received: " + ctx.method() + " " + ctx.path());
+        });
 
         app.before(ctx -> {
            String id = ctx.queryParam("id");
