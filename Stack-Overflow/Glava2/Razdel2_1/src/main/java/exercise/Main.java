804f4992ecc2964423d86b120d2796c7d625b11d
CherepovAlex <westler123@yandex.ru>
2025-07-04 14:53:32 +0300

    Commit 2025-07-04 page 24

--- Stack-Overflow/Glava2/Razdel2_1/src/main/java/exercise/Main.java
+++ Stack-Overflow/Glava2/Razdel2_1/src/main/java/exercise/Main.java
@@ -2,6 +2,22 @@ package exercise;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Неявное приведение
+        byte byteVar = 42;
+        short shortVar = byteVar;
+        int intVar = shortVar;
+        long longVar = intVar;
+        float floatVar = longVar;
+        double doubleVar = floatVar;
+
+        // Явное приведение
+        double doubleVar1 = 42.0d;
+        float floatVar1 = (float) doubleVar1;
+        long longVar1 = (long) floatVar1;
+        int intVar1 = (int) longVar1;
+        short shortVar1 = (short) intVar1;
+        byte byteVar1 = (byte) shortVar1;
+
+
     }
 }
