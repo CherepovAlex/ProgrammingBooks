3d5cc42a1bb3708dc19a457e12afe94ca47b2100
CherepovAlex <westler123@yandex.ru>
2025-07-04 16:07:23 +0300

    Commit 2025-07-04 page 24

--- Stack-Overflow/Glava2/Razdel2_1/src/main/java/exercise/Main.java
+++ Stack-Overflow/Glava2/Razdel2_1/src/main/java/exercise/Main.java
@@ -18,6 +18,19 @@ public class Main {
         short shortVar1 = (short) intVar1;
         byte byteVar1 = (byte) shortVar1;
 
+        char char1 = 1, char2 = 2;
+        short short1 = 1, short2 = 2;
+        int int1 = 1, int2 = 2;
+        float float1 = 1.0f, float2 = 2.0f;
+
+        // char1 = char1 + char2;       // Ошибка: Невозможно преобразовать из int в char;
+        // short1 = short1 + short2;    // Ошибка: Невозможно преобразовать из int в short;
+        int1 = char1 + char2;           // char преобразуется в int.
+        int1 = short1 + short2;         // short переводится в int.
+        int1 = char1 + short2;          // и char, и short преобразуются в int.
+        float1 = short1 + float2;       // short преобразуется в float
+        int1 = int1 + int2;             // int не изменяется.
+
 
     }
 }
