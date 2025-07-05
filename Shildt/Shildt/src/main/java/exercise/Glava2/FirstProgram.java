f334643f2c6b3ffae1b7b27794ba504e8e874222
CherepovAlex <westler123@yandex.ru>
2025-07-05 19:22:10 +0300

    Commit 2025-07-05 page 32

---
+++ Shildt/Shildt/src/main/java/exercise/Glava2/FirstProgram.java
@@ -0,0 +1,9 @@
+package exercise.Glava2;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Simple program on Java!");
+    }
+}
+
+// Simple program on Java!
---
+++ Shildt/Shildt/src/main/java/exercise/Glava2/SecondProgram.java
@@ -0,0 +1,15 @@
+package exercise.Glava2;
+
+public class SecondProgram {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("Value num: " + num);
+        num = num * 2;
+        System.out.print("Value num * 2: ");
+        System.out.println(num);
+    }
+}
+
+// Значение num: 100
+// Значение num  *  2: 200
