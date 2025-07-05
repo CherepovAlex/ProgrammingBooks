a29887e1e4f4679bbb824b78628432bb27e2b59b
CherepovAlex <westler123@yandex.ru>
2025-07-05 09:13:07 +0300

    Commit 2025-07-05 page 29

--- Stack-Overflow/Glava 3.md
+++ Stack-Overflow/Glava 3.md
@@ -74,7 +74,7 @@ public class CountHolder {
     private int count = 0;
     
     public synchronized int getCount() {return count;}
-    public synchronized void setCount() { count = c;}
+    public synchronized void setCount(int c) { count = c;}
 }
 ```
 
--- Stack-Overflow/Glava3/Glava3/src/main/java/exercise/Main.java
+++ Stack-Overflow/Glava3/Glava3/src/main/java/exercise/Main.java
@@ -1,9 +1,64 @@
 package exercise;
 
-//TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
-// нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
 public class Main {
     public static void main(String[] args) {
 
+    }
+}
+
+class Person {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        if (name != null && name.length() > 2)
+            this.name = name;
+    }
+
+    public String getName1() {
+    if (getName().length() > 16)
+        return "Слишком длинное имя!";
+    else
+        return name;
+}
+}
+
+class CountHolder {
+    private int count = 0;
+
+    public int getCount() { return count;}
+    public void setCount(int c) { count = c;}
+}
+
+class CountHolder1 {
+    public int count = 0;
+}
+
+class CountHolder2 {
+    private int count = 0;
+
+    public synchronized int getCount() {return count;}
+    public synchronized void setCount(int c) { count = c;}
+}
+
+class Sample {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
