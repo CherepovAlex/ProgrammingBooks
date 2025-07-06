4661777e64ac116c2ec3a49c4a812ac941889d73
CherepovAlex <westler123@yandex.ru>
2025-07-06 19:52:14 +0300

    Commit 2025-07-06 page 35

---
+++ Shildt/Shildt/src/main/java/exercise/Glava3/Area.java
@@ -0,0 +1,13 @@
+package exercise.Glava3;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+
+        r = 10.8;       // радиус круга
+        pi = 3.14;      // приближенное значение Pi
+        a = pi * r * r; // вычислить площадь
+
+        System.out.println("Area of circle is " + a);
+    }
+}
