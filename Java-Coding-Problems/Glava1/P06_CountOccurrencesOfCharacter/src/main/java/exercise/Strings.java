37912c250d8583f00e1266788d932c9c0b34cacf
CherepovAlex <westler123@yandex.ru>
2025-07-28 19:40:30 +0300

    Commit 2025-07-28 11.2

--- Java-Coding-Problems/Glava1/P06_CountOccurrencesOfCharacter/src/main/java/exercise/Strings.java
+++ Java-Coding-Problems/Glava1/P06_CountOccurrencesOfCharacter/src/main/java/exercise/Strings.java
@@ -7,25 +7,20 @@ public class Strings {
     }
 
     public static int countOccurrencesOfACertainCharacterV1(String str, char ch) {
-
         if (str == null || str.isBlank()) {
             // or throw IllegalArgumentException
             return -1;
         }
-
         return str.length() - str.replace(String.valueOf(ch), "").length();
     }
 
 
     public static int countOccurrencesOfACertainCharacterV2(String str, char ch) {
-
         if (str == null || str.isBlank()) {
             // or throw IllegalArgumentException
             return -1;
         }
-
         int count = 0;
-
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == ch) {
                 count++;
@@ -36,12 +31,10 @@ public class Strings {
 
 
     public static long countOccurrencesOfACertainCharacterV3(String str, char ch) {
-
         if (str == null || str.isBlank()) {
             // or throw IllegalArgumentException
             return -1;
         }
-
         return str.chars()
                 .filter((c -> c == ch))
                 .count();
@@ -53,29 +46,22 @@ public class Strings {
             // or throw IllegalArgumentException
             return -1;
         }
-
         if (ch.codePointCount(0, ch.length()) > 1) {
             return -1; // there is more than 1 Unicode character in the given String
         }
-
         int result = str.length() - str.replace(ch, "").length();
-
         // if ch.length() return 2 then this is Unicode surrogate pair
         return ch.length() == 2 ? result / 2 : result;
-
     }
 
     public static int countOccurrencesOfACertainCharacterVCP2(String str, String ch) {
-
         if (str == null || ch == null || str.isEmpty() || str.isEmpty()) {
             // or throw IllegalArgumentException
             return -1;
         }
-
         if (ch.codePointCount(0, ch.length()) > 1) {
             return -1; // there is more than 1 Unicode character in the given String
         }
-
         int count = 0;
         int codePoint = ch.codePointAt(0);
 
@@ -89,18 +75,14 @@ public class Strings {
 
 
     public static long countOccurrencesOfACertainCharacterVCP3(String str, String ch) {
-
         if (str == null || ch == null || str.isEmpty() || str.isEmpty()) {
             // or throw IllegalArgumentException
             return -1;
         }
-
         if (ch.codePointCount(0, ch.length()) > 1) {
             return -1; // there is more than 1 Unicode character in the given String
         }
-
         int codePoint = ch.codePointAt(0);
-
         return str.codePoints()
                 .filter(c -> c == codePoint)
                 .count();
