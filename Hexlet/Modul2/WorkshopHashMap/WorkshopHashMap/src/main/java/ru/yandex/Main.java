420fd7b6768c7fd47413b42fe402f8cb6eee887b
CherepovAlex <westler123@yandex.ru>
2025-11-07 15:16:33 +0300

    Commit 2025-11-06 workshop hashmap2

--- Hexlet/Modul2/WorkshopHashMap/WorkshopHashMap/src/main/java/ru/yandex/Main.java
+++ Hexlet/Modul2/WorkshopHashMap/WorkshopHashMap/src/main/java/ru/yandex/Main.java
@@ -29,5 +29,6 @@ public class Main {
         map2.put(null, null);
 
 
+
     }
 }
---
+++ Hexlet/Modul2/WorkshopHashMap/WorkshopHashMap/src/main/java/ru/yandex/Main2.java
@@ -0,0 +1,7 @@
+package ru.yandex;
+
+public class Main2 {
+    public static void main(String[] args) {
+        // 1
+    }
+}
